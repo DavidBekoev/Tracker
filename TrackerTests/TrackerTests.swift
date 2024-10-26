dc8c3e79db101768cf8a06a2b52591ae5e7bb129
Давид <bekoevdawid@yandex.ru>
2024-10-26 17:04:07 +0300

    создание проекта

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Давид Бекоев on 25.10.2024.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
